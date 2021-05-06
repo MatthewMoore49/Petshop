541c1f35c7e412b145b60367ff4b6ca795584b36
Matt <msm3390@gmail.com>
2021-05-06 09:46:06 -0400

    push

--- Services.js
+++ Services.js
@@ -2,9 +2,16 @@ var service = [
     {
       title: 'Pet Grooming',
       image: './img/gerbile.jpg',
-      price: '$7,500-15,000'
-      description
+      price: '$7,500-15,000',
+      description: "hello"
     },
+
+    {
+      title: 'Pet Grooming',
+      image: './Princess.jpg',
+      price: '$7,500-15,000',
+      description: "hello"
+    }
     
   
     
@@ -16,13 +23,13 @@ var service = [
    
   
   
-    for (var i=0; i < ; i++){
-        var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
-        var image = '<img src="' + shop[i] + '"/'
-        var price = '<p> $' + .price + '</p></span>'
-        var description = '<div class=""><p>'+ .description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+    for (var i=0; i < service.length; i++){
+        var heading = '<div class="product ' + '"><span><h5>' + service[i].title + '</h5>'
+        var image = '<img src="' + service[i].image + '"/'
+        var price = '<p> $' + service[i].price + '</p></span>'
+        var description = '<div class=""><p>'+ service[i].description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
         var concatThis = heading + image + price + description;
         postHTML = postHTML + concatThis
     }
-    document.getElementById('market').innerHTML = postHTML
+    document.getElementById('services').innerHTML = postHTML
   
